e856442488f2d13d7d31039a6e04980c533b4902
Nazihah Ahwan <nazihah.ahwan8992@gmail.com>
2021-11-02 14:45:40 +0800

    Advance State Management with Context

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import {BlogProvider} from "./src/context/BlogContext";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import IndexScreen from "./src/screens/IndexScreen";
+
+const navigator = createStackNavigator({
+  Index: IndexScreen
+},{
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
 }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+  <BlogProvider><App/></BlogProvider>
+  );
+};
